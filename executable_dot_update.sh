82f35ba9d57840f2ae1414b0ce24135b422283b0
EntityDeleter <uiop2028@gmail.com>
2025-09-13 13:21:19 +0530

    Add .update.sh

---
+++ executable_dot_update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Update Repositories Recursively:
+sudo pacman -Syu --config ~/.pacmanrepofiles/core.conf --noconfirm |& grep -v "is newer than"
+sudo pacman -Syu --config ~/.pacmanrepofiles/core-testing.conf --noconfirm |& grep -v "is newer than"
+sudo pacman -Syu --config ~/.pacmanrepofiles/extra.conf --noconfirm |& grep -v "is newer than"
+sudo pacman -Syu --config ~/.pacmanrepofiles/extra-testing.conf --noconfirm |& grep -v "is newer than"
+sudo pacman -Syu --config ~/.pacmanrepofiles/multilib.conf --noconfirm |& grep -v "is newer than"
+sudo pacman -Syu --config ~/.pacmanrepofiles/multilib-testing.conf --noconfirm |& grep -v "is newer than"
+sudo pacman -Syu --config ~/.pacmanrepofiles/cachyos.conf --noconfirm |& grep -v "is newer than"
+sudo pacman -Syu --config ~/.pacmanrepofiles/endeavouros.conf --noconfirm |& grep -v "is newer than"
+
+# Update:
+topgrade
+arch-update -d -s
+
+# Update Package Lists:
+~/.diff.sh
