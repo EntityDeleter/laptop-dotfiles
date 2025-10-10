36a33e7f5ab468e8e77d743eb17b35ea640b0d8b
EntityDeleter <uiop2028@gmail.com>
2025-10-10 19:16:15 +0530

    Update .update.sh

--- executable_dot_update.sh
+++ executable_dot_update.sh
@@ -17,7 +17,7 @@ arch-update -d -s
 # Cleanup:
 sudo rm -rf ~/.cache/* /tmp/*
 conda clean --all
-nix-store --gc | rg -vi "deleting "
+#nix-store --gc | rg -vi "deleting "
 
 # Update Package Lists:
 ~/.diff.sh
