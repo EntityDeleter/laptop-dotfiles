2fac5bea3da17dadeb7512d4a758ff7f11f94e2c
EntityDeleter <uiop2028@gmail.com>
2025-11-04 17:21:36 +0530

    Update .update.sh

--- executable_dot_update.sh
+++ executable_dot_update.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # Update Repositories Recursively:
-sudo pacman -Syu --config ~/.pacmanrepofiles/core.conf --noconfirm |& grep -v "is newer than"
-sudo pacman -Syu --config ~/.pacmanrepofiles/core-testing.conf --noconfirm |& grep -v "is newer than"
-sudo pacman -Syu --config ~/.pacmanrepofiles/extra.conf --noconfirm |& grep -v "is newer than"
-sudo pacman -Syu --config ~/.pacmanrepofiles/extra-testing.conf --noconfirm |& grep -v "is newer than"
-sudo pacman -Syu --config ~/.pacmanrepofiles/multilib.conf --noconfirm |& grep -v "is newer than"
-sudo pacman -Syu --config ~/.pacmanrepofiles/multilib-testing.conf --noconfirm |& grep -v "is newer than"
-sudo pacman -Syu --config ~/.pacmanrepofiles/cachyos.conf --noconfirm |& grep -v "is newer than"
-sudo pacman -Syu --config ~/.pacmanrepofiles/endeavouros.conf --noconfirm |& grep -v "is newer than"
+sudo-rs pacman -Syu --config ~/.pacmanrepofiles/core.conf --noconfirm |& grep -v "is newer than"
+sudo-rs pacman -Syu --config ~/.pacmanrepofiles/core-testing.conf --noconfirm |& grep -v "is newer than"
+sudo-rs pacman -Syu --config ~/.pacmanrepofiles/extra.conf --noconfirm |& grep -v "is newer than"
+sudo-rs pacman -Syu --config ~/.pacmanrepofiles/extra-testing.conf --noconfirm |& grep -v "is newer than"
+sudo-rs pacman -Syu --config ~/.pacmanrepofiles/multilib.conf --noconfirm |& grep -v "is newer than"
+sudo-rs pacman -Syu --config ~/.pacmanrepofiles/multilib-testing.conf --noconfirm |& grep -v "is newer than"
+sudo-rs pacman -Syu --config ~/.pacmanrepofiles/cachyos.conf --noconfirm |& grep -v "is newer than"
+sudo-rs pacman -Syu --config ~/.pacmanrepofiles/endeavouros.conf --noconfirm |& grep -v "is newer than"
 
 # Update:
 topgrade
 arch-update -d -s
 
 # Cleanup:
-sudo rm -rf ~/.cache/* /tmp/*
+sudo-rs rm -rf ~/.cache/* /tmp/*
 conda clean --all
 #nix-store --gc | rg -vi "deleting "
 
