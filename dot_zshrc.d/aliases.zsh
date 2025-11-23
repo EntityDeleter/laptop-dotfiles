2b581791a4d07125721c08a4ce631546447d32f5
EntityDeleter <uiop2028@gmail.com>
2025-11-23 20:10:44 +0530

    Update .zshrc.d/aliases.zsh

--- dot_zshrc.d/aliases.zsh
+++ dot_zshrc.d/aliases.zsh
@@ -41,5 +41,3 @@ alias ll="eza --icons=always --color=always --hyperlink -l -b --smart-group -h -
 alias lla="eza --icons=always --color=always --hyperlink -l -b --smart-group -h -m -M --time-style=relative -a"
 alias lld="eza --icons=always --color=always --hyperlink -l -b --smart-group -h -m -M --time-style=relative -T"
 alias llda="eza --icons=always --color=always --hyperlink -l -b --smart-group -h -m -M --time-style=relative -T -a"
-
-alias rg="rg --color=auto --heading -n -P -p --trim "
