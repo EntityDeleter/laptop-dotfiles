25054bcf65383d66d1dbd80200efde804ed98c72
EntityDeleter <uiop2028@gmail.com>
2025-12-19 15:50:29 +0530

    Update .zshrc.d/aliases.zsh

--- dot_zshrc.d/aliases.zsh
+++ dot_zshrc.d/aliases.zsh
@@ -14,8 +14,6 @@ alias openssh="ssh -p 39901 "
 alias tailssh="ssh "
 
 alias btrfs-assistant='pkexec env WAYLAND_DISPLAY="$XDG_RUNTIME_DIR/$WAYLAND_DISPLAY" XDG_RUNTIME_DIR="$XDG_RUNTIME_DIR" QT_QPA_PLATFORM=wayland btrfs-assistant'
-alias orange3='conda activate orange3 && QT_QPA_PLATFORM=xcb python -m Orange.canvas'
-alias rich="conda activate rich-cli && rich --pager "
 alias pdf="tdf "
 alias cat="mcat -t vscode -s --report --opts 'center=true,inline=false' -a "
 alias icat="mcat -t vscode -s --report --opts 'center=true,inline=false' -a -o interactive "
@@ -25,17 +23,9 @@ cl() {
   eza --icons=always --color=always --hyperlink
 }
 
-alias colorprint="lp -d CanonG3000 -o COlorModel=RGB "
-alias blackprint="lp -d CanonG3000 -o raw -o ColorModel=Gray "
-
 alias ffw='kitty zsh -c "fastfetch -c ~/.config/fastfetch/left.jsonc; fastfetch -c ~/.config/fastfetch/right.jsonc; sleep 10m"'
 
 alias pingtest="gping 127.0.0.1 192.168.1.1 1.1.1.1 google.com"
-
-alias nix="nix --extra-experimental-features nix-command --extra-experimental-features flakes "
-
-alias wireup="sudo tailscale down; sudo systemctl stop tailscaled; sudo wg-quick up wlanUS1"
-alias tailup="sudo wg-quick down wlanUS1; sudo systemctl start tailscaled; sudo tailscale up"
 
 alias ls="eza --icons=always --color=always --hyperlink"
 alias la="eza --icons=always --color=always --hyperlink -a"
