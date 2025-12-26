26826dd25804fdd556978014a62a80272245d9b1
EntityDeleter <uiop2028@gmail.com>
2025-12-26 14:06:04 +0530

    Update .p10k.zsh

--- dot_p10k.zsh
+++ dot_p10k.zsh
@@ -169,7 +169,7 @@
   fi
 
   # Default background color.
-  typeset -g POWERLEVEL9K_BACKGROUND=236
+  typeset -g POWERLEVEL9K_BACKGROUND=238
 
   # Separator between same-color segments on the left.
   typeset -g POWERLEVEL9K_LEFT_SUBSEGMENT_SEPARATOR='%244F\u2502'
