ac443edc57b0ad80b391ab197298732606d29653
EntityDeleter <uiop2028@gmail.com>
2026-02-10 11:43:36 +0530

    Update .zshrc.d/aliases.zsh

--- dot_zshrc.d/aliases.zsh
+++ dot_zshrc.d/aliases.zsh
@@ -14,28 +14,26 @@ alias openssh="ssh -p 39901 "
 alias tailssh="ssh "
 alias mosh="mosh --ssh='ssh -p 39901' "
 
-alias btrfs-assistant='pkexec env WAYLAND_DISPLAY="$XDG_RUNTIME_DIR/$WAYLAND_DISPLAY" XDG_RUNTIME_DIR="$XDG_RUNTIME_DIR" QT_QPA_PLATFORM=wayland btrfs-assistant'
-alias pdf="tdf "
 alias cat="mcat -t vscode -s --report --opts 'center=true,inline=false' -a "
 alias icat="mcat -t vscode -s --report --opts 'center=true,inline=false' -a -o interactive "
 
 cl() {
   z "$1"
-  eza --icons=always --color=always --hyperlink
+  eza --icons=always --hyperlink
 }
 
 alias ffw='kitty zsh -c "fastfetch -c ~/.config/fastfetch/left.jsonc; fastfetch -c ~/.config/fastfetch/right.jsonc; sleep 10m"'
 
 alias pingtest="gping 127.0.0.1 192.168.1.1 1.1.1.1 google.com"
 
-alias ls="eza --icons=always --color=always --hyperlink"
-alias la="eza --icons=always --color=always --hyperlink -a"
-alias ld="eza --icons=always --color=always --hyperlink -T"
-alias lda="eza --icons=always --color=always --hyperlink -T -a"
-alias ll="eza --icons=always --color=always --hyperlink -l -b --smart-group -h -m -M --time-style=relative"
-alias lla="eza --icons=always --color=always --hyperlink -l -b --smart-group -h -m -M --time-style=relative -a"
-alias lld="eza --icons=always --color=always --hyperlink -l -b --smart-group -h -m -M --time-style=relative -T"
-alias llda="eza --icons=always --color=always --hyperlink -l -b --smart-group -h -m -M --time-style=relative -T -a"
+alias ls="eza --icons=always --hyperlink"
+alias la="eza --icons=always --hyperlink -a"
+alias ld="eza --icons=always --hyperlink -T"
+alias lda="eza --icons=always --hyperlink -T -a"
+alias ll="eza --icons=always --hyperlink -l -b --smart-group -h -m -M --time-style=relative"
+alias lla="eza --icons=always --hyperlink -l -b --smart-group -h -m -M --time-style=relative -a"
+alias lld="eza --icons=always --hyperlink -l -b --smart-group -h -m -M --time-style=relative -T"
+alias llda="eza --icons=always --hyperlink -l -b --smart-group -h -m -M --time-style=relative -T -a"
 
 alias rg="rga --color=auto --heading -n -P -p --trim"
 
@@ -52,3 +50,5 @@ alias signoff='ARCHWEB_PASSWORD="$(sudo \cat /root/.arch-signoff-password)" sign
 getsdf() {
   curl -o "/mnt/Data/Avogadro2/Imports/${1}.sdf" "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/${1}/SDF"
 }
+
+alias scan='scanimage -d "pixma:G3000_192.168.1.12" -p --resolution=600 -o "
