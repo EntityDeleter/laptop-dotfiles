8948320e9b0c0b238be7a5fbac618e69ed3e4a26
EntityDeleter <uiop2028@gmail.com>
2026-03-16 16:51:40 +0530

    Update .update.sh

--- executable_dot_update.sh
+++ executable_dot_update.sh
@@ -5,9 +5,6 @@ yay
 
 # Update Repositories Recursively:
 sudo-rs pacman -Syu --config ~/.pacmanrepofiles/cachyos.conf --noconfirm |& grep -v "is newer than"
-sudo-rs pacman -Syu --config ~/.pacmanrepofiles/core-testing.conf --noconfirm |& grep -v "is newer than"
-sudo-rs pacman -Syu --config ~/.pacmanrepofiles/extra-testing.conf --noconfirm |& grep -v "is newer than"
-sudo-rs pacman -Syu --config ~/.pacmanrepofiles/multilib-testing.conf --noconfirm |& grep -v "is newer than"
 sudo-rs pacman -Syu --config ~/.pacmanrepofiles/core.conf --noconfirm |& grep -v "is newer than"
 sudo-rs pacman -Syu --config ~/.pacmanrepofiles/extra.conf --noconfirm |& grep -v "is newer than"
 sudo-rs pacman -Syu --config ~/.pacmanrepofiles/multilib.conf --noconfirm |& grep -v "is newer than"
